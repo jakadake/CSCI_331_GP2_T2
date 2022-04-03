b17123ad8e56c5e02537496698851364f9d056fd
jakadake <jahappo@gmail.com>
2022-04-03 14:43:04 -0500

    Add project files.

---
+++ Source/CSCI_331_GP2_T2/CSCI_331_GP2_T2.cpp
@@ -0,0 +1,12 @@
+﻿// CSCI_331_GP2_T2.cpp : Defines the entry point for the application.
+//
+
+#include "CSCI_331_GP2_T2.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
